b10bf5a759ef500cac6d85708d1765fab2683f65
Daniil <danya.moiseev@inbox.ru>
2023-08-21 17:55:45 +1000

    price-list n accordeon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html", "./src/index.html"],
+  content: [
+    "./dist/index.html", "./src/index.html", 
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
   theme: {
     extend: {
       fontFamily: {
         main: ['Montserrat'],
-      }
+      },
+      
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
 }
 
